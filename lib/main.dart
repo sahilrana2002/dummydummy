227861f36d59e49f424b64043e801ff4e3ab4a87
nikirana22 <nikirana1983@gmail.com>
2023-09-02 08:45:52 +0530

    renamed class from MyApp to A

--- lib/main.dart
+++ lib/main.dart
@@ -30,18 +30,17 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const A(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class A extends StatefulWidget {
+  const A({super.key, required this.title});
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
   // how it looks.
-
   // This class is the configuration for the state. It holds the values (in this
   // case the title) provided by the parent (in this case the App widget) and
   // used by the build method of the State. Fields in a Widget subclass are
@@ -50,10 +49,10 @@ class MyHomePage extends StatefulWidget {
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<A> createState() => _AState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _AState extends State<A> {
   int _counter = 0;
 
   void _incrementCounter() {
